11aa9c48e73b8768184765f1ef2ee8cfb0c738ca
geraldinenasa <you@example.com>
2023-08-19 21:43:44 -0300

    Correccion Endpoint reparto

--- server.js
+++ server.js
@@ -1,95 +1,4 @@
 
-//este endpoint retorna el json TRAILERFLIX
-app.get('/catalogo', (req, res) => { //ver si hay que darle estilo o como
-    res.json(TRAILERFLIX);
-});
-
-
-//este endpoint retorna un elemento si lo encuentra en la lista de TRAILERFLIX y si no, retorna un error
-app.get('/trailer/:id', (req, res) => {
-    const id = parseInt(req.params.id.trim());
-    let title = TRAILERFLIX.find(title => title.id === id);
-    if (title != undefined) {
-        const data = {
-            id: title.id,
-            titulo: title.titulo,
-            trailer: title.trailer?.trim()
-        };
-        res.json(data);
-    } else {
-        res.status(404).json({ "Error": "No se encontraron titulos que contengan el id indicado" });
-    }
-});
-
-// // const dotenv = require('dotenv');
-// const express = require('express');
-// // const bodyParser = require('body-parser');
-// const app = express();
-// const path = require("path");
-
-// // Inicializamos  el Motor de plantillas elegido 
-// app.set('view engine', 'ejs')
-// app.use(express.static('view'))
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // const { leerFrutas,guardarFrutas,  obtenerFrutaFind,actualizarFruta, eliminarFruta } = require('./src/frutas.manager')
-// const PORT = process.env.PORT || 3009;
-// let DB = [];
-// const fs = require('fs');
-
-// // process.env.DATABASE_PATH ME TIRA ERROR 
-// const datos = fs.readFileSync(__dirname + '/src/database/trailerflix.json', 'utf8' )
-// const TRAILERFLIX = JSON.parse(datos)
-// // console.log(TRAILERFLIX);
-
-// app.get("/", (req, res) => { 
-//     // res.send('Bienvenido a nuestro sitio generado a partir de un motor de plantillas.')
-//     //RUTA DINAMICA 
-//     const data = {
-//         title: 'BIENVENIDO',
-//         message: 'Bienvenido a la primer entrega',
-//         name: 'Backend - Ingenias',
-//     }
-//     res.render('index', data)
-// }); 
-
-// //Retorna el archivo json
-// app.get('/catalogo', (req,res) => {
-//     res.json(TRAILERFLIX);
-// });
-
-//Endpoint: llamado /titulo/:title que liste el catálogo de películas y/o series que se 
-//aproxime al título enviado. (la búsqueda del nombre debe ser parcial)
-
-app.get('/titulo/:title', (req, res) => {
-    const nombre = req.params.title.trim().toLowerCase();
-    const filtro = TRAILERFLIX.filter(filtro => filtro.titulo.toLowerCase().includes(nombre));
-
-    if (filtro !== []) {
-        res.json(filtro)
-    } else {
-        res.status(404).send("El título que busca no se encuentra disponible en nuestra plataforma.");
-    }
-});
-
-//Endpoint: Para el endpoint /categoria/:cat utiliza también .filter() y retorna todos los resultados 
-//encontrados. (Aquí son dos posibles valores solamente)
-
-app.get('/categoria/:cat', (req, res) => {
-    const cate = req.params.cat.trim().toLowerCase();
-    const nombre = TRAILERFLIX.filter(nombre => nombre.categoria.toLowerCase() == cate);
-
-
-    if (nombre !== []) {
-        res.json(nombre);
-    } else {
-        res.status(404).send("El título que busca no se encuentra en la categoria");
-    }
-});
-
-app.listen(PORT, () => {
-    console.log(`API is listening on port ${PORT}`);
-});
 
 // const dotenv = require('dotenv');
 const express = require('express');
@@ -109,6 +18,7 @@ const fs = require('fs');
 const datos = fs.readFileSync(__dirname + '/src/database/trailerflix.json', 'utf8' )
 const TRAILERFLIX = JSON.parse(datos)
 // console.log(TRAILERFLIX);
+
 
 
 app.get("/", (req, res) => { 
@@ -141,10 +51,54 @@ app.get("/", (req, res) => {
 }); 
 
 
+//este endpoint retorna el json TRAILERFLIX
+app.get('/catalogo', (req, res) => { //ver si hay que darle estilo o como
+    res.json(TRAILERFLIX);
+});
 
 
-app.listen(PORT, () => { 
-    console.log(`API is listening on port ${PORT}`); 
+//este endpoint retorna un elemento si lo encuentra en la lista de TRAILERFLIX y si no, retorna un error
+app.get('/trailer/:id', (req, res) => {
+    const id = parseInt(req.params.id.trim());
+    let title = TRAILERFLIX.find(title => title.id === id);
+    if (title != undefined) {
+        const data = {
+            id: title.id,
+            titulo: title.titulo,
+            trailer: title.trailer?.trim()
+        };
+        res.json(data);
+    } else {
+        res.status(404).json({ "Error": "No se encontraron titulos que contengan el id indicado" });
+    }
+});
+
+
+
+app.get('/titulo/:title', (req, res) => {
+    const nombre = req.params.title.trim().toLowerCase();
+    const filtro = TRAILERFLIX.filter(filtro => filtro.titulo.toLowerCase().includes(nombre));
+
+    if (filtro !== []) {
+        res.json(filtro)
+    } else {
+        res.status(404).send("El título que busca no se encuentra disponible en nuestra plataforma.");
+    }
+});
+
+//Endpoint: Para el endpoint /categoria/:cat utiliza también .filter() y retorna todos los resultados 
+//encontrados. (Aquí son dos posibles valores solamente)
+
+app.get('/categoria/:cat', (req, res) => {
+    const cate = req.params.cat.trim().toLowerCase();
+    const nombre = TRAILERFLIX.filter(nombre => nombre.categoria.toLowerCase() == cate);
+
+
+    if (nombre !== []) {
+        res.json(nombre);
+    } else {
+        res.status(404).send("El título que busca no se encuentra en la categoria");
+    }
 });
 
 
@@ -152,21 +106,25 @@ app.listen(PORT, () => {
 //(Como resultado, retorna solo un array con la propiedad “reparto” y la propiedad “titulo” 
 //y sus respectivos datos (no devuelvas todo el contenido) ¿recuerdas a .map()?
 
-app.get('/reparto/:act', (req, res)=>{
+app.get('/reparto/:act', (req, res) => {
     const act = req.params.act.trim().toLowerCase();
-    const reparto = leerPeliculas().filter(reparto => reparto.act = act);
+    const reparto = TRAILERFLIX.filter(pelicula => pelicula.reparto.toLowerCase().includes(act));
 
-//Retorna los datos del reparto     
-
-if(reparto !==[]){
-        const reparto1 = reparto.map(x =>{
-            return{
-                titulo: x.titulo,
-                reparto: x.reparto,
-            }
-        })
-        res.json(reparto1.includes(act))
+    if (reparto.length > 0) {
+        const resultado = reparto.map(pelicula => {
+            return {
+                titulo: pelicula.titulo,
+                reparto: pelicula.reparto
+            };
+        });
+        res.json(resultado);
     } else {
-        res.status(404).send('No se encuentra un actor/actriz con ese nombre')
-}
+        res.status(404).send('No se encuentra un actor/actriz con ese nombre en el reparto de ninguna película.');
+    }
 })
+
+app.listen(PORT, () => { 
+    console.log(`API is listening on port ${PORT}`); 
+});
+
+
